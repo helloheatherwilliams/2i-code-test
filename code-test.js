a1cc3c3f5b9f790344926afbf3fa745b4bd59623
helloheatherw <heather.williams@teachfirst.org.uk>
2023-06-08 11:57:59 +0100

    Add assumptions and additional test

--- code-test.js
+++ code-test.js
@@ -1,6 +1,9 @@
 // “In a language of your choice, using all best practice principles you’re aware of, could you please provide code that reads a list of 10 numbers ranging between 1 to 100.
 // Assuming there are 4 duplicates in the list of numbers, the output should remove the duplicates and sort the remaining numbers in descending order. Please state any assumptions you’ve made.
 
+// Assumption: Array contains only numbers
+// Assumption: Array does not contain big ints
+
 const sort = (arr) => arr.sort((a, b) => b - a);
 
 const removeDuplicates = (arr) => {
--- test.js
+++ test.js
@@ -7,6 +7,14 @@ describe("sort", () => {
         const result = sort(exampleInput);
 
         expect(result).toEqual(expectedOutput);
+    });
+
+    test("returns a list of numbers in descending order including negatives", () => {
+        const exampleInput = [1, 2, 3, -4, 4, 5, 6, 7, 8, 9, 10];
+        const expectedOutput = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -4];
+        const result = sort(exampleInput);
+
+        expect(result).toEqual(expectedOutput);
     })
 })
 
@@ -14,6 +22,14 @@ describe("removeDuplicates", () => {
     test("returns a list of numbers that are not duplicated", () => {
         const exampleInput = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10];
         const expectedOutput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const result = removeDuplicates(exampleInput);
+ 
+        expect(result).toEqual(expectedOutput);
+    });
+
+    test("when given an empty array returns an empty array", () => {
+        const exampleInput = [];
+        const expectedOutput = [];
         const result = removeDuplicates(exampleInput);
  
         expect(result).toEqual(expectedOutput);
